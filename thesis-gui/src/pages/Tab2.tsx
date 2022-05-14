f4234263d8a3bf5691559fab091c078ace0b4250
Ela Cymerys <ela.cymerys@gmail.com>
2022-05-14 18:56:39 +0200

    Refactor code

--- thesis-gui/src/pages/Tab2.tsx
+++ thesis-gui/src/pages/Tab2.tsx
@@ -7,71 +7,71 @@ import {
     IonLabel,
     IonList,
     IonListHeader,
-    IonPage,
+    IonPage, IonRadioGroup,
     IonTitle,
     IonToolbar
 } from '@ionic/react';
 import './Tab2.css';
+import React from "react";
+
+const mockCategories = [
+    "Category 1",
+    "Category 2",
+    "Category 3",
+    "Category 4",
+    "Category 5",
+    "Category 6",
+    "Category 7",
+];
+
+type Category = {
+    name: string;
+};
+
+const Category: React.FC<Category> = props => {
+    return (
+        <IonItem>
+            <IonLabel>
+                { props.name }
+            </IonLabel>
+            <IonCheckbox slot="start" />
+        </IonItem>
+    );
+}
+
+const categoryItems = mockCategories.map(category =>
+    <Category name={ category } />
+);
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 2</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 2</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
 
-          <IonList>
-              <IonListHeader>Categories</IonListHeader>
+                <IonList>
+                    <IonRadioGroup>
+                        <IonListHeader>
+                            <IonLabel>Categories</IonLabel>
+                        </IonListHeader>
 
-              <IonItem>
-                  <IonLabel>Category 1</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
+                        { categoryItems }
+                    </IonRadioGroup>
+                </IonList>
 
-              <IonItem>
-                  <IonLabel>Category 2</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
-
-              <IonItem>
-                  <IonLabel>Category 3</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
-
-              <IonItem>
-                  <IonLabel>Category 4</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
-
-              <IonItem>
-                  <IonLabel>Category 5</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
-
-              <IonItem>
-                  <IonLabel>Category 6</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
-
-              <IonItem>
-                  <IonLabel>Category 7</IonLabel>
-                  <IonCheckbox slot="start" />
-              </IonItem>
-          </IonList>
-
-          <IonButton expand="block">Choose</IonButton>
-
-      </IonContent>
-    </IonPage>
-  );
+                <IonButton expand="block">Choose</IonButton>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab2;
