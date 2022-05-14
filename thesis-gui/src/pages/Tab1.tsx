363dcb1c3562f89e328fd354e899319b7a39ae38
Ela Cymerys <ela.cymerys@gmail.com>
2022-05-14 12:12:24 +0200

    Create schema for displaying question

--- thesis-gui/src/pages/Tab1.tsx
+++ thesis-gui/src/pages/Tab1.tsx
@@ -1,8 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+    IonButton,
+    IonCard, IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle, IonCardTitle,
+    IonContent,
+    IonHeader, IonItem, IonLabel, IonList,
+    IonPage, IonRadio, IonRadioGroup,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import './Tab1.css';
+import {useState} from "react";
 
 const Tab1: React.FC = () => {
+  const [selected, setSelected] = useState<string>(null!);
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +28,57 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+          <IonCard>
+              <IonCardHeader>
+                  <IonCardSubtitle>
+                      Category
+                  </IonCardSubtitle>
+                  <IonCardTitle>
+                      Question X
+                  </IonCardTitle>
+              </IonCardHeader>
+
+              <IonCardContent style={{ textAlign: "justify" }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Libero nunc consequat.
+              </IonCardContent>
+          </IonCard>
+
+          <IonList>
+              <IonRadioGroup value={selected} onIonChange={e => setSelected(e.detail.value)}>
+                  <IonItem>
+                      <IonLabel>
+                          Answer 1
+                      </IonLabel>
+                      <IonRadio slot="start" value="ans1" />
+                  </IonItem>
+
+                  <IonItem>
+                      <IonLabel>
+                          Answer 2
+                      </IonLabel>
+                      <IonRadio slot="start" value="ans2" />
+                  </IonItem>
+
+                  <IonItem>
+                      <IonLabel>
+                          Answer 3
+                      </IonLabel>
+                      <IonRadio slot="start" value="ans3" />
+                  </IonItem>
+
+                  <IonItem>
+                      <IonLabel>
+                          Answer 4
+                      </IonLabel>
+                      <IonRadio slot="start" value="ans4" />
+                  </IonItem>
+              </IonRadioGroup>
+          </IonList>
+
+          <IonButton expand="block">Check</IonButton>
+
       </IonContent>
     </IonPage>
   );
