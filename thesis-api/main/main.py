c251080dfb8b0cf83e9149e17856b2204fe6f969
Ela Cymerys <ela.cymerys@gmail.com>
2022-05-18 17:43:55 +0200

    Create endpoint for getting all categories

--- thesis-api/main/main.py
+++ thesis-api/main/main.py
@@ -1,6 +1,10 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from starlette.status import HTTP_200_OK
+
+from services.category_service import CategoryService
+from web.schemas import CategoryListResponse, CategoryResponse
 
 app = FastAPI()
 
@@ -16,3 +20,14 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/api/categories", status_code=HTTP_200_OK, response_model=CategoryListResponse)
+async def get_categories(service: CategoryService = Depends()):
+    categories = service.get_all()
+
+    res = []
+    for category in categories:
+        res.append(CategoryResponse(id=category.id, name=category.name))
+
+    return CategoryListResponse(categories=res)
