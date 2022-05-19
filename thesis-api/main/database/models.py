7f8a0c1cc7601b228fae9412cfe7f3f24228be4f
Thonem <kowalczykmkonrad@gmail.com>
2022-05-19 18:55:59 +0200

    Add term model

--- thesis-api/main/database/models.py
+++ thesis-api/main/database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 from . import Base, engine
 
@@ -13,6 +14,18 @@ class CategoryModel(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, unique=True, nullable=False)
     search_word = Column(String, unique=True, nullable=False)
+
+    terms = relationship("TermModel")
+
+
+class TermModel(Base):
+    __tablename__ = "terms"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, unique=True, nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+
+    category = relationship("CategoryModel", back_populates="terms")
 
 
 Base.metadata.create_all(engine)
