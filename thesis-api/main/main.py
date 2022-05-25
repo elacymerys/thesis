e59e63c0e4d4599c350b5dd0b95b44f5ec9186c9
lukasz <Lozoko2000@interia.pl>
2022-05-25 12:44:41 +0200

    category fixes

--- thesis-api/main/main.py
+++ thesis-api/main/main.py
@@ -17,6 +17,7 @@ logging.info(f'Initialize database {DB_NAME}')
 with contextmanager(get_db)() as db:
     category_service = CategoryService.build(db)
     category_service.add_all(fn='../categories.txt')
+    db.commit()
 
 logging.info(f'Add all categories from file to database {DB_NAME}')
 
--- thesis-api/main/services/category_service.py
+++ thesis-api/main/services/category_service.py
@@ -18,12 +18,21 @@ class CategoryService:
 
     def add_all(self, fn: str):
         with open(fn, mode='r') as csv_file:
+            categories_in_db = self.__dao.get_all()
+            categories_in_db_names = [category.name for category in categories_in_db]
+            categories_in_db_search_word = [category.search_word for category in categories_in_db]
+
             csv_reader = csv.DictReader(csv_file, delimiter=';')
-            for row in csv_reader:
-                if self.__dao.exists_with_name(row['name']):
-                    continue
-                category = Category(name=row['name'], search_word=row['search_word'])
-                self.__dao.create(category)
+            categories_from_csv = [Category(name=row['name'], search_word=row['search_word']) for row in csv_reader]
+
+            categories_to_update = [category for category in categories_from_csv if
+                                    category.name in categories_in_db_names
+                                    and category.search_word !=
+                                    categories_in_db_search_word[categories_in_db_names.index(category.name)]]
+            new_categories = [category for category in categories_from_csv
+                              if category.name not in categories_in_db_names]
+            self.__dao.update_search_words(categories_to_update)
+            self.__dao.save_all(new_categories)
 
     def get_all(self) -> List[Category]:
         return self.__dao.get_all()
