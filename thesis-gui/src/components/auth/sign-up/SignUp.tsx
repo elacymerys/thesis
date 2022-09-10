d771e64bcb69e0776b7c8da9a0ed0c699ab09a64
Ela Cymerys <ela.cymerys@gmail.com>
2022-09-10 11:00:34 +0200

    Add sign up plain form

---
+++ thesis-gui/src/components/auth/sign-up/SignUp.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    IonButton,
+    IonContent, IonHeader, IonInput,
+    IonPage, IonRouterLink, IonTitle, IonToolbar,
+} from "@ionic/react";
+import './SignUp.css';
+
+const SignUp: React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Sign Up</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Sign Up</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonInput type="email" required placeholder="Email" />
+                <IonInput type="password" required placeholder="Password" />
+                <IonButton expand="full">Sign Up</IonButton>
+                <IonRouterLink href="/auth/sign-in">Already have an account? Sign in</IonRouterLink>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default SignUp;
