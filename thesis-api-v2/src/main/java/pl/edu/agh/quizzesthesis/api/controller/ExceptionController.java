f2b25a96e6bdb2d699626c9a121d7ebb10c3124b
Thonem <kowalczykmkonrad@gmail.com>
2022-10-08 16:28:45 +0200

    THE-42 Authentication - add 409 on nick or email conflict

--- thesis-api-v2/src/main/java/pl/edu/agh/quizzesthesis/api/controller/ExceptionController.java
+++ thesis-api-v2/src/main/java/pl/edu/agh/quizzesthesis/api/controller/ExceptionController.java
@@ -1,6 +1,5 @@
 package pl.edu.agh.quizzesthesis.api.controller;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.edu.agh.quizzesthesis.api.dto.TextResponse;
+import pl.edu.agh.quizzesthesis.business.exception.ConflictException;
 import pl.edu.agh.quizzesthesis.business.exception.NotFoundException;
 
 @ControllerAdvice
@@ -22,21 +22,21 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({AuthenticationException.class})
-    public ResponseEntity<TextResponse> handleAuthenticationError() {
+    public ResponseEntity<TextResponse> handleAuthenticationException() {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new TextResponse("User unauthorized"));
     }
 
-    @ExceptionHandler({DataIntegrityViolationException.class})
-    public ResponseEntity<TextResponse> handleDBConflict() {
+    @ExceptionHandler({ConflictException.class})
+    public ResponseEntity<TextResponse> handleConflict() {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(new TextResponse("Violating data integrity"));
+                .body(new TextResponse("Resource conflict"));
     }
 
     @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity<TextResponse> resourceNotFound(NotFoundException e) {
+    public ResponseEntity<TextResponse> handleNotFound(NotFoundException e) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new TextResponse(e.getMessage()));
