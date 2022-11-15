e604a1dcc75641833394d5a9a07ce9e56bafe854
elacymerys <ela.cymerys@gmail.com>
2022-11-15 19:04:31 +0100

    Handle sign out on My Account page

--- thesis-gui/src/components/my-account/MyAccount.tsx
+++ thesis-gui/src/components/my-account/MyAccount.tsx
@@ -9,14 +9,11 @@ import {
     IonToolbar
 } from "@ionic/react";
 import {userService} from "../../services/user-service";
+import {useUserContext} from "../../context/UserContext";
 
-const UserInfo: React.FC<{
-    nick: string,
-    email: string
-}> = ({
-    nick,
-    email
-}) => {
+const UserInfo: React.FC = () => {
+    const { user, signOut } = useUserContext();
+
     return (
         <IonCard>
             <IonCardContent style={{ textAlign: "center" }}>
@@ -25,13 +22,13 @@ const UserInfo: React.FC<{
                 </IonAvatar>
                 <IonCardContent>
                     <IonCardTitle>
-                        { nick }
+                        { user!.nick }
                     </IonCardTitle>
                     <IonCardSubtitle>
-                        { email }
+                        { user!.email }
                     </IonCardSubtitle>
                 </IonCardContent>
-                <IonButton expand="block">
+                <IonButton expand="block" onClick={ signOut }>
                     Sign Out
                 </IonButton>
             </IonCardContent>
@@ -94,7 +91,7 @@ export const MyAccount: React.FC = () => {
                         <IonTitle>My Account</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                <UserInfo nick={ nick } email={ email } />
+                <UserInfo />
                 <Ranking />
             </IonContent>
         </IonPage>
