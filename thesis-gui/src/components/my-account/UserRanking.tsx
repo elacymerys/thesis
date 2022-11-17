6254871eb79a150431149c8cba9f7d38356ffb0d
elacymerys <ela.cymerys@gmail.com>
2022-11-17 10:12:12 +0100

    Refactor code

---
+++ thesis-gui/src/components/my-account/UserRanking.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+    IonCard,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonRouterLink
+} from "@ionic/react";
+import {useUserContext} from "../../context/UserContext";
+import {useCategoryContext} from "../../context/CategoryContext";
+
+const CategoryRanksItem: React.FC<{ name: string, rank: number }> = ({ name, rank }) => {
+    return (
+        <IonItem>
+            <IonLabel>{ name }</IonLabel>
+            <IonLabel slot="end">
+                { rank }
+            </IonLabel>
+        </IonItem>
+    );
+}
+
+export const UserRanking = () => {
+    const { user } = useUserContext();
+    const { categories } = useCategoryContext();
+
+    const getCategoryNameWithId = (id: number) => {
+        for (let category of categories) {
+            if (category.id === id) {
+                return category.name;
+            }
+        }
+        return '';
+    }
+
+    const categoryRanksItems = Object.entries(user!.categoryRanks).filter(([_, rank]) => rank > 0).map(([id, rank]) => {
+        return <CategoryRanksItem
+            name={ getCategoryNameWithId(parseInt(id)) }
+            rank={ rank }
+        />
+    });
+
+    return (
+        <IonCard>
+            <IonCardContent>
+                <IonCardTitle style={{ textAlign: "center" }}>
+                    My Ranking
+                </IonCardTitle>
+                {
+                    categoryRanksItems.length > 0 ?
+                        <IonList lines="full">
+                            { categoryRanksItems }
+                        </IonList> :
+                        <>
+                            <IonCardSubtitle style={{ textAlign: "center" }}>
+                                Play to earn points and climb the leaderboard!
+                            </IonCardSubtitle>
+                            <IonCardSubtitle style={{ textAlign: "center" }}>
+                                Click <IonRouterLink routerLink="/categories">here</IonRouterLink> to start the game
+                            </IonCardSubtitle>
+                        </>
+                }
+            </IonCardContent>
+        </IonCard>
+    );
+}
