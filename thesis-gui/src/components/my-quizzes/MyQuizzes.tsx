976845200c8a2b74dc11851fd5056ba673d9f9d8
elacymerys <ela.cymerys@gmail.com>
2022-11-19 11:58:52 +0100

    Refactor My Quizzes component code

--- thesis-gui/src/components/my-quizzes/MyQuizzes.tsx
+++ thesis-gui/src/components/my-quizzes/MyQuizzes.tsx
@@ -2,42 +2,56 @@ import React from "react";
 import {
     IonButton,
     IonContent,
-    IonHeader,
     IonItem,
     IonLabel,
     IonList,
-    IonPage,
-    IonTitle,
-    IonToolbar
+    IonPage
 } from "@ionic/react";
+import {PageHeader} from "../common/PageHeader";
+
+const PAGE_NAME = "My Quizzes";
+
+const QUIZZES = [
+    {
+        name: "Quiz 1",
+        questionsNumber: 5
+    },
+    {
+        name: "Quiz 2",
+        questionsNumber: 27
+    },
+    {
+        name: "Quiz 3",
+        questionsNumber: 8
+    },
+    {
+        name: "Quiz 4",
+        questionsNumber: 13
+    }
+];
+
+const QuizzesListItem: React.FC<{
+    name: string,
+    questionsNumber: number
+}> = ({ name, questionsNumber }) => {
+    return (
+        <IonItem button detail={true}>
+            <IonLabel>
+                <h2>{ name }</h2>
+                <p>{ `${questionsNumber} question(s)` }</p>
+            </IonLabel>
+        </IonItem>
+    );
+}
 
 const QuizzesList: React.FC = () => {
-  return (
-      <IonList lines="full">
-          <IonItem button detail={true}>
-              <IonLabel>
-                  <h2>Quiz 1</h2>
-                  <p>x questions</p>
-              </IonLabel>
-          </IonItem>
-          <IonItem button detail={true}>
-              <IonLabel>
-                  <h2>Quiz 2</h2>
-                  <p>x questions</p>
-              </IonLabel>
-          </IonItem>
-          <IonItem button detail={true}>
-              <IonLabel>
-                  <h2>Quiz 3</h2>
-                  <p>x questions</p>
-              </IonLabel>
-          </IonItem>
-          <IonItem button detail={true}>
-              <IonLabel>
-                  <h2>Quiz 4</h2>
-                  <p>x questions</p>
-              </IonLabel>
-          </IonItem>
+    const quizzesListItems = QUIZZES.map(quiz => {
+        return <QuizzesListItem name={ quiz.name } questionsNumber={ quiz.questionsNumber } />
+    });
+
+    return (
+      <IonList lines="full" style={{ paddingTop: "15px" }}>
+          { quizzesListItems }
       </IonList>
   );
 }
@@ -45,17 +59,9 @@ const QuizzesList: React.FC = () => {
 const MyQuizzes: React.FC = () => {
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar style={{ textAlign: "center" }}>
-                    <IonTitle>My Quizzes</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+            <PageHeader name={ PAGE_NAME } condense={ false } />
             <IonContent className="ion-padding">
-                <IonHeader collapse="condense">
-                    <IonToolbar style={{ textAlign: "center" }}>
-                        <IonTitle>My Quizzes</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
+                <PageHeader name={ PAGE_NAME } condense={ true } />
                 <IonButton expand="block">
                     Create new quiz
                 </IonButton>
