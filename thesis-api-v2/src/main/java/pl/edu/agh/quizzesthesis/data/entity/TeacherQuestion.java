de792239da6e7993ecdf9d0be07720e14b04c514
lukasz <Lozoko2000@interia.pl>
2022-11-21 20:04:19 +0100

    fix id generation for TeacherQuestion

--- thesis-api-v2/src/main/java/pl/edu/agh/quizzesthesis/data/entity/TeacherQuestion.java
+++ thesis-api-v2/src/main/java/pl/edu/agh/quizzesthesis/data/entity/TeacherQuestion.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class TeacherQuestion {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String question;
