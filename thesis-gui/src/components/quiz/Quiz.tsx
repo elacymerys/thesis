e19832048ef7c871b3bc707e2d792b55f122bd0e
elacymerys <76401381+elacymerys@users.noreply.github.com>
2022-12-08 16:08:16 +0100

    Disable button if no item is chosen

--- thesis-gui/src/components/quiz/Quiz.tsx
+++ thesis-gui/src/components/quiz/Quiz.tsx
@@ -119,7 +119,7 @@ export const Quiz: React.FC = () => {
 
                 <IonButton
                     onClick={ checkAnswer }
-                    disabled={ showResult }
+                    disabled={ selected === undefined || showResult }
                     expand="block"
                     style={{ marginTop: 20, marginBottom: 30 }}
                 >
